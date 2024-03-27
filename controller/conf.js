f1b2d0e572deb7dff7909651d44e6ed97a133c78
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-27 16:05:50 +0530

    Second commit

--- controller/conf.js
+++ controller/conf.js
@@ -75,16 +75,6 @@
 // //     deleteConf,
 // // };
 
-
-
-
-
-
-
-
-
-
-
 // const conf = require("../models/conf");
 // const user = require("../models/user");
 
@@ -173,97 +163,90 @@
 //     deleteConf,
 // };
 
-
-
-
-
-
-
-
 const conf = require("../models/conf");
 const user = require("../models/user");
 
 const newConf = async (req, res) => {
-    const { name, email, campus, phoneNo, image } = req.body;
-    console.log(req.body);
-    try {
-        const findUser = await user.findOne({ email: email });
-        console.log(findUser);
-        if (!findUser) throw new Error("User not found");
+  const { name, email, campus, phoneNo, image } = req.body;
+  console.log(req.body);
+  try {
+    const findUser = await user.findOne({ email: email });
+    console.log(findUser);
+    if (!findUser) return res.status(404).json({ message: "User not found" });
 
-        const newConf = new conf({
-            name,
-            email,
-            campus,
-            phoneNo,
-            image,
-            accepted: findUser.role === "teacher" ? true : false,
-        });
-        await newConf.save();
-        if (!newConf) throw new Error("An error occured");
-        res.json(newConf);
-    } catch (e) {
-        res.send("Error");
-    }
+    const newConf = new conf({
+      name,
+      email,
+      campus,
+      phoneNo,
+      image,
+      accepted: findUser.role === "teacher" ? true : false,
+    });
+    await newConf.save();
+    if (!newConf) throw new Error("An error occured");
+    res.json(newConf);
+  } catch (e) {
+    res.status(500).json({ message: "Error" });
+  }
 };
 
 const getConf = async (req, res) => {
-    try {
-        const conff = await conf.find();
-        if (!conff) throw new Error("No conf found");
-        res.json(conff);
-    } catch (e) {
-        console.log(e);
-        res.send("Error");
-    }
+  try {
+    const conff = await conf.find();
+    if (!conff) throw new Error("No conf found");
+    res.json(conff);
+  } catch (e) {
+    console.log(e);
+    res.send("Error");
+  }
 };
 
 const getid = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!id) throw new Error("Id is required");
-        const conff = await conf.find({ email: id });
-        if (!conff) throw new Error("No conf found");
-        console.log(conff);
-        res.send(conff);
-    } catch (e) {
-        res.send("Error");
-    }
+  const { id } = req.params;
+  try {
+    if (!id) throw new Error("Id is required");
+    const conff = await conf.find({ email: id });
+    if (!conff) throw new Error("No conf found");
+    console.log(conff);
+    res.send(conff);
+  } catch (e) {
+    res.send("Error");
+  }
 };
 
 const updateConf = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!id) throw new Error("Id is required");
-        const conff = await conf.findByIdAndUpdate(
-            id,
-            { accepted: true },
-            { new: true }
-        );
-        if (!conff) throw new Error("An error occured");
-        // console.log(conff);
-        res.send(conff);
-    } catch (e) {
-        res.send("Error");
-    }
+  const { id } = req.params;
+  try {
+    if (!id) throw new Error("Id is required");
+    const conff = await conf.findByIdAndUpdate(
+      id,
+      { accepted: true },
+      { new: true }
+    );
+    if (!conff) throw new Error("An error occured");
+    // console.log(conff);
+    res.send(conff);
+  } catch (e) {
+    res.send("Error");
+  }
 };
 
 const deleteConf = async (req, res) => {
-    const { id } = req.params;
-    try {
-        if (!id) throw new Error("Id is required");
-        const conff = await conf.findByIdAndDelete(id);
-        if (!conff) throw new Error("An error occured");
-        res.send(conff);
-    } catch (e) {
-        res.send("Error");
-    }
+  const { id } = req.params;
+  try {
+    if (!id) throw new Error("Id is required");
+    const conff = await conf.findByIdAndDelete(id);
+    if (!conff) throw new Error("An error occured");
+    res.send(conff);
+  } catch (e) {
+    res.send("Error");
+  }
 };
 
 module.exports = {
-    newConf,
-    getConf,
-    getid,
-    updateConf,
-    deleteConf,
+  newConf,
+  getConf,
+  getid,
+  updateConf,
+  deleteConf,
 };
